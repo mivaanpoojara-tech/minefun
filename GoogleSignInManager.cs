c48c5eb6d34502bead893b971359a70672bdb839
mivaanpoojara-tech <poojaramivaan@gmail.com>
2026-05-01 19:10:43 +0530

    Create GoogleSignInManager.cs

---
+++ GoogleSignInManager.cs
@@ -0,0 +1,9 @@
+// GoogleSignInManager.cs - manages the Google sign-in process
+
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine;
+
+public class GoogleSignInManager : MonoBehaviour {
+    // Your Google Sign-in methods here
+}
